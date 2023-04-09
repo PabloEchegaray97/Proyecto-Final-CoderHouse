2fcd99227f3d5e0782a951bd2eb2d94878c293c9
pae <pablo.echegaray97@gmail.com>
2023-04-09 10:44:32 -0300

    fix scripts & min-height: 100vh on contacto.html

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ const nav = window.addEventListener("scroll", function() {
     const nav = document.querySelector(".bg-menu");
     nav.classList.toggle("sticky", window.scrollY>0);
 })
+const btn = document.getElementById('button');
+
+    document.getElementById('form')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            btn.value = 'Sending...';
+
+            const serviceID = 'default_service';
+            const templateID = 'template_97fs1bt';
+
+            emailjs.sendForm(serviceID, templateID, this)
+                .then(() => {
+                    btn.value = 'Send Email';
+                    alert('Enviado. ¡Gracias por confiar en nosotros!');
+                }, (err) => {
+                    btn.value = 'Send Email';
+                    alert(JSON.stringify(err));
+                });
+        });
